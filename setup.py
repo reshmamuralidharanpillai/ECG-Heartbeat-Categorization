0d5544f98c2324e472be21ad9cf8aae7f8a9b7d5
Reshma M <reshmamuralidharanpillai@gmail.com>
2023-05-30 12:33:06 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Heartbest Categorization',
+    author='Reshma M',
+    license='MIT',
+)
